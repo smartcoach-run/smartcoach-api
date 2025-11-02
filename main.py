09f1301e3c8a6dcec9944a3d8bbe7ab1f02699ea
smartcoach-run <jerome.achte@gmail.com>
2025-11-02 16:51:15 +0100

    Reset de la valeur du PORT à 3000

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ def generate_by_id():
     return jsonify({"fields": fields})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
