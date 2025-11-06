29340e258eb02c05aadbd13d9293389ad68eb14b
smartcoach-run <jerome.achte@gmail.com>
2025-11-06 10:37:32 +0100

    Update main.py
    Correction mineure

--- main.py
+++ main.py
@@ -486,5 +486,4 @@ def generate_by_id():
 # -----------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    port = int(_env("PORT", "5000"))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=5000)
